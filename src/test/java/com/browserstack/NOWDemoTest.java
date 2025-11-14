0772f5e3a5e88530418a195b34bacd454043e945
Samiran Saha <samiran.s89@gmail.com>
2025-11-14 16:55:00 +0530

    print yml

--- src/test/java/com/browserstack/NOWDemoTest.java
+++ src/test/java/com/browserstack/NOWDemoTest.java
@@ -3,10 +3,15 @@ import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import com.browserstack.SeleniumTest;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class NOWDemoTest extends SeleniumTest {
     @Test
     public void openTestPage() throws Exception {
+
+        String content = new String(Files.readAllBytes(Paths.get("browserstack.yml")));
+        System.out.println(content);
         
         String webDomainFromEnv = System.getenv("NOW_WEB_DOMAIN");
         System.out.println("Web Domain from Env: " + webDomainFromEnv);
